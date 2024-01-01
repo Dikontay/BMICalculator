0c8dd9b9f5a341100de1aea95cb7463fe2b501d8
Dikontay <dalimbayeva@icloud.com>
2024-01-01 20:20:49 +0600

    need to complete calculation

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const app = express()
 
 const port = 3000
 
+// app.set('view engine', 'ejs');
+// app.set('views', templatePath.join(__dirname, 'views')); // Assuming your EJS files will be in a 'views' folder
+
+
 app.use(bodyParser.urlencoded({extended : false}))
+
+app.use(express.static('public')); // assuming your static files are in a 'public' folder
+
 
 app.get('/', (req, res) => {
     res.sendFile(templatePath.join(__dirname + '/home.html'))
@@ -17,13 +24,20 @@ app.get('/', (req, res) => {
 app.get('/bmicalculator', (req, res)=>{res.sendFile(templatePath.join(__dirname + '/bmi.html'))})
 
 
-app.post('/bmicalculator', (req, res)=>{
-    let age = req.body.inputAge
-    let height = req.body.inputHeight
-    let weight = req.body.inputWeight
-    console.log(age)
+app.post('/bmicalculator', (req, res) => {
+    let age = req.body.inputAge;
+    let height = req.body.inputHeight;
+    let weight = req.body.inputWeight;
+    let unit = String(req.body.height);
+    let result;
+    if (unit == "(cm)") {
+        result = calculateBMIMetric(weight, height);
+    } else {
+        result = calculateBMIImperial(weight, height);
+    }
+    //  res.send(String(result))
+});
 
-})
 
 function calculateBMIMetric(weight, height) {
     return weight / ((height / 100) ** 2);
