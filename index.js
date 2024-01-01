376878fa71705eafa59d38602bdec4b529fd5772
Dikontay <dalimbayeva@icloud.com>
2024-01-01 16:10:21 +0600

    feat : implement post method

--- index.js
+++ index.js
@@ -16,4 +16,21 @@ app.get('/', (req, res) => {
 
 app.get('/bmicalculator', (req, res)=>{res.sendFile(templatePath.join(__dirname + '/bmi.html'))})
 
+
+app.post('/bmicalculator', (req, res)=>{
+    let age = req.body.inputAge
+    let height = req.body.inputHeight
+    let weight = req.body.inputWeight
+    console.log(age)
+
+})
+
+function calculateBMIMetric(){
+
+}
+
+function calculateBMIImperial(){
+
+}
+
 app.listen(port, () => console.log(`Listening port in http://localhost:${port}/`))
